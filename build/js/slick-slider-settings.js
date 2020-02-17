d7ee1a0491a7704594d16226c3121f6ad2f8644b
AdiStas91 <adistas91@yandex.ru>
2020-02-17 17:19:27 +0300

    fix

--- build/js/slick-slider-settings.js
+++ build/js/slick-slider-settings.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
  	$('.product-groups__list').slick({
  	slidesToShow: 3,
-  	slidesToScroll: 3,
+  	slidesToScroll: 1,
   	responsive: [
     	{
       		breakpoint: 660,
       			settings: {
         			slidesToShow: 1,
         			slidesToScroll: 1,
-        			infinite: true,
       			}
     	},
     	{
       		breakpoint: 1280,
       			settings: {
         			slidesToShow: 2,
-        			slidesToScroll: 2
+        			slidesToScroll: 1
       			}
     	},
     ]
